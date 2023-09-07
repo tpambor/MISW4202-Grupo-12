23090d086a15caf68a4fb70c994fbe537d5fa365
lvoj90 <lvoj90@gmail.com>
2023-09-07 07:33:35 -0500

    Ajuste cola de mensajes y empresa proyecto.

--- ExperimentoABCJobs/cola_mensajes/cola_mensajes.py
+++ ExperimentoABCJobs/cola_mensajes/cola_mensajes.py
@@ -1,12 +1,32 @@
 from celery import Celery
 from celery.signals import task_postrun
+import requests
+import json
 
 celery_app = Celery(__name__, broker='redis://localhost:6379/0')
-
+     
 @celery_app.task(name='search_best_candidates')
-def search__best_candidates(id_vacancy, vacancyjson):
-     pass
+def search_best_candidates(id_vacancy):
+    send_to_voting(id_vacancy)
+
+def send_to_voting(id_vacancy):
+    voting_url = "http://localhost:5000/voting" 
+
+    data = {
+        "id_vacancy": id_vacancy
+    }
+
+    try:
+        response = requests.post(voting_url, json=data)
+
+        if response.status_code == 200:
+            print(f"Solicitud enviada a Voting {id_vacancy}")
+        else:
+            print(f"Error al enviar la solicitud a Voting: {response.status_code}")
+    except Exception as e:
+        print(f"Error al enviar la solicitud a Voting: {str(e)}")
+
 
 @task_postrun.connect()
 def close_session(*args, **kwargs):
-     pass
+    pass
--- ExperimentoABCJobs/empresa_proyecto/app.py
+++ ExperimentoABCJobs/empresa_proyecto/app.py
@@ -1,6 +1,8 @@
 
 from empresa_proyecto import create_app
 from celery import Celery
+from cola_mensajes import search_best_candidates 
+from time import sleep 
 
 celery_app = Celery(__name__, broker="redis://localhost:6379/0")
 
@@ -9,7 +11,7 @@ app_context = app.app_context()
 app_context.push()
 
 @celery_app.task(name="response_best_candidates")
-def response_best_candidates(response):
+def response_best_candidates(response):\
    print(response)
 
 
@@ -20,12 +22,16 @@ def registrar_log(id_vacancy, best_candidate):
 
 @celery_app.task(name="request_best_candidates")
 def request_best_candidates(id_vacancy):
-    pass
+    search_best_candidates.apply_async(args=[id_vacancy])
 
-def publish_message(index):
-    args = (index,)
-    request_best_candidates.apply_async(args)
 
-for index in range(1, 10):
-    publish_message(index)
+def publish_messages(num_messages):
+    for index in range(1, num_messages + 1):
+        args = (index,)
+        request_best_candidates.apply_async(args)
+        print(f"Enviada solicitud {index} a la cola de mensajes.")
+        sleep(1)  
 
+if __name__ == "__main__":
+    num_messages = 1000  
+    publish_messages(num_messages)
