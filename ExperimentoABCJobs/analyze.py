582de00167087cffc0e4c0f9e99a1991c9050e71
Tim Pambor <t.pambor@uniandes.edu.co>
2023-09-09 02:34:01 +0200

    Actualizar script de analysis

--- ExperimentoABCJobs/analyze.py
+++ ExperimentoABCJobs/analyze.py
@@ -8,21 +8,21 @@ motor_1_producido = set()
 motor_2_producido = set()
 motor_3_producido = set()
 
-with open('logs/voting.csv', newline='') as csvfile:
+with open('evidencia/run3/voting.csv', newline='') as csvfile:
     data = csv.DictReader(csvfile)
     voting = [*data]
 
-with open('logs/motor_emparejamiento_1.csv', newline='') as csvfile:
+with open('evidencia/run3/motor_emparejamiento_1.csv', newline='') as csvfile:
     data = csv.DictReader(csvfile)
     for row in data:
         motor_1_producido.add(int(row['id_vacante']))
 
-with open('logs/motor_emparejamiento_2.csv', newline='') as csvfile:
+with open('evidencia/run3/motor_emparejamiento_2.csv', newline='') as csvfile:
     data = csv.DictReader(csvfile)
     for row in data:
         motor_2_producido.add(int(row['id_vacante']))
 
-with open('logs/motor_emparejamiento_3.csv', newline='') as csvfile:
+with open('evidencia/run3/motor_emparejamiento_3.csv', newline='') as csvfile:
     data = csv.DictReader(csvfile)
     for row in data:
         motor_3_producido.add(int(row['id_vacante']))
@@ -43,7 +43,7 @@ motor_1_falso_positivo = motor_1_detectado - motor_1_producido
 motor_1_falso_negativo = motor_1_producido - motor_1_detectado
 print(f"Detectados correctos: {len(motor_1_identificados)}")
 print(f"Falsos positivos: {len(motor_1_falso_positivo)}")
-print(f"Falsos negativos: {len(motor_1_falso_negativo)}")
+print(f"Falsos negativos: {len(motor_1_falso_negativo)} {motor_1_falso_negativo}")
 
 print("")
 print("--- MotorEmparejamiento 2 ---")
@@ -54,7 +54,7 @@ motor_2_falso_positivo = motor_2_detectado - motor_2_producido
 motor_2_falso_negativo = motor_2_producido - motor_2_detectado
 print(f"Detectados correctos: {len(motor_2_identificados)}")
 print(f"Falsos positivos: {len(motor_2_falso_positivo)}")
-print(f"Falsos negativos: {len(motor_2_falso_negativo)}")
+print(f"Falsos negativos: {len(motor_2_falso_negativo)} {motor_2_falso_negativo}")
 
 print("")
 print("--- MotorEmparejamiento 3 ---")
@@ -65,7 +65,7 @@ motor_3_falso_positivo = motor_3_detectado - motor_3_producido
 motor_3_falso_negativo = motor_3_producido - motor_3_detectado
 print(f"Detectados correctos: {len(motor_3_identificados)}")
 print(f"Falsos positivos: {len(motor_3_falso_positivo)}")
-print(f"Falsos negativos: {len(motor_3_falso_negativo)}")
+print(f"Falsos negativos: {len(motor_3_falso_negativo)} {motor_3_falso_negativo}")
 
 print("")
 print("--- Total ---")
@@ -75,3 +75,23 @@ total_falso_negativo = len(motor_1_falso_negativo) + len(motor_2_falso_negativo)
 print(f"Detectados correctos: {total_identificados}")
 print(f"Falsos positivos: {total_falso_positivo}")
 print(f"Falsos negativos: {total_falso_negativo}")
+
+un_error = len(motor_1_identificados - motor_2_identificados - motor_3_identificados) \
+         + len(motor_2_identificados - motor_1_identificados - motor_3_identificados) \
+         + len(motor_3_identificados - motor_1_identificados - motor_2_identificados)
+
+dos_errores = len(motor_1_identificados & motor_2_identificados - motor_3_identificados) \
+            + len(motor_1_identificados & motor_3_identificados - motor_2_identificados) \
+            + len(motor_3_identificados & motor_2_identificados - motor_1_identificados)
+
+tres_errores = len(motor_1_identificados & motor_2_identificados & motor_3_identificados)
+
+print("")
+print("--- Vacantes ---")
+print(f"Con un error:          {un_error} (enmascarados)")
+print(f"Con dos errores:        {dos_errores}")
+print(f"Con tres errores:        {tres_errores}")
+print(f"Con al menos un error: {un_error+dos_errores+tres_errores}")
+
+print("")
+print(f"Errores enviado a EmpresaProyecto: {dos_errores+tres_errores}")
