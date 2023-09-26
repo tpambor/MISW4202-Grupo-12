054b1c9385a6ad6e5ca36d98cc4d0bb92a1157bc
lvoj90 <lvoj90@gmail.com>
2023-09-26 16:59:08 -0500

    Ajuste Validar usuario con token

--- ExperimentoSeguridadABCJobs/AdminContrato/app.py
+++ ExperimentoSeguridadABCJobs/AdminContrato/app.py
@@ -1,6 +1,7 @@
 from flask_restful import  Api
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import re
+import jwt
 
 app = Flask(__name__)  
 app_context = app.app_context()
@@ -10,25 +11,46 @@ api = Api(app)
 
 # Diccionario de datos con contratos y usuarios
 contratos = [
-    {"userId": "empleado_1", "contratoId": 1, "aspiranteId": 1},
-    {"userId": "empleado_2", "contratoId": 2, "aspiranteId": 2},
-    {"userId": "empleado_3", "contratoId": 3, "aspiranteId": 3},
-    {"userId": "empleado_4", "contratoId": 4, "aspiranteId": 4},
+    {"contratoId": 1, "userId": "empleado_1"},
+    {"contratoId": 1, "userId": "aspirante_1"},
+    {"contratoId": 2, "userId": "empleado_2"},
+    {"contratoId": 2, "userId": "aspirante_2"},
+    {"contratoId": 3, "userId": "empleado_3"},
+    {"contratoId": 3, "userId": "aspirante_3"},
+    {"contratoId": 4, "userId": "empleado_4"},
+    {"contratoId": 4, "userId": "aspirante_4"},
 ]
-    
+
+clave_secreta = "secret"
+
 @app.route('/contrato/<int:contrato_id>', methods=['PUT'])
 def editar_contrato(contrato_id):
     contrato = next((c for c in contratos if c['contratoId'] == contrato_id), None)
-
+    
     if contrato is None:
         return jsonify({"error": "Contrato no encontrado"}), 404
-
-    numero = re.search(r'\d+', contrato['userId']).group()
     
-    if  str(contrato['contratoId']) == numero:
-        return jsonify({"mensaje": f"El contrato {contrato_id} pertenece al usuario {numero}"}), 200
+    tokenRequest = request.json["token"]
+    token = tokenRequest.get(token)
+    
+    try:
+    # Verificar y decodificar el token
+        payload = jwt.decode(token, clave_secreta, algorithms=["HS256"])
+        usuario = payload.get('usuario')
+        print("usuario:", usuario)
+    except jwt.InvalidTokenError:
+        print("El token no es válido.")
+        
+    if not usuario.startswith("empleado_"):
+        return jsonify({"error": "El usuario no es un empleado"}), 403
+    
+    print(contrato['contratoId'])
+    print(usuario[len("empleado_")])
+    if str(contrato['contratoId']) ==  usuario[len("empleado_"):]:
+        return jsonify({"mensaje": f"El contrato {contrato_id} pertenece al usuario { contrato['userId']}"}), 200
     else:
         return jsonify({"error": "El contrato no pertenece al usuario especificado"}), 403
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5002)
