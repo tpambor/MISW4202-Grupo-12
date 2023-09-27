eb3d409a798ed2b0966f4feadd3100dd9ca04ed5
Tim Pambor <t.pambor@uniandes.edu.co>
2023-09-27 19:35:34 +0200

    Fix Api Gateway

--- ExperimentoSeguridadABCJobs/ApiGateway/app.py
+++ ExperimentoSeguridadABCJobs/ApiGateway/app.py
@@ -11,21 +11,20 @@ api = Api(app)
 
 class VistaLogin(Resource):
     def post(self):
-        resLogin = requests.post("http://127.0.0.1:5001/login", data={'usuario': request.json["usuario"]})
+        resLogin = requests.post("http://127.0.0.1:5001/login", json={'usuario': request.json["usuario"]})
         return resLogin.json(), resLogin.status_code
     
 class VistaContrato(Resource):
     def put(self, id_contrato):
-        token = request.json["token"]
-        resValidate = requests.post("http://127.0.0.1:5001/validate", data={'token': token})
+        auth_header = request.headers.get('Authorization', '')
+        headers = {'Authorization': auth_header}
+
+        resValidate = requests.get("http://127.0.0.1:5001/validate", headers=headers)
         if resValidate.status_code == 200:
-            resContrato = requests.put("http://127.0.0.1:5002/contrato/{}".format(id_contrato), data={'token': token})
+            resContrato = requests.put("http://127.0.0.1:5002/contrato/{}".format(id_contrato), headers=headers, json=request.json)
             return resContrato.json(), resContrato.status_code
         else:
             return resValidate.json(), resValidate.status_code
-            
-        
-      
 
 api.add_resource(VistaLogin, "/login")
 api.add_resource(VistaContrato, "/contrato/<int:id_contrato>")
